d49fe82049db30f473b95516bb58a45f91eb8478
Valeriy_Voblikov <103932538+ketynkyn@users.noreply.github.com>
2023-03-14 20:03:36 +0300

    grayscale +

--- src/App.js
+++ src/App.js
@@ -2,29 +2,42 @@ import React, {useEffect, useState} from 'react';
 import './ChoosePhoto.scss';
 
 
-
-
 function App() {
-
+  const[reset ,setReset] = useState(0)
+  const [grayscale, setGrayscale] = useState(false)
   const [image,setImage] = useState([])
   const [imageUrls,setImageUrls] = useState([])
   const ChangeImage = (event) => {
     setImage([...event.target.files])
-    console.log(event.target.files)
   }
-
-  useEffect(() =>{
+  useEffect(() => {
     if (image.length < 1) return;
     const newImageUrls = [];
     image.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
     setImageUrls(newImageUrls);
   }, [image]);
 
+  const[slider, setSlider] = useState(100);
+  const changeGrayscale = () => {
+    setGrayscale(true)
+    document.getElementById("img").style.filter="grayscale("+slider+"%)"
+  }
+
+
   return (
       <div className="App">
-        <input type="file" id="file" accept="image/*" onChange={ChangeImage}/>
-        <div id="img-area" className="box"> {imageUrls.map(img=> <img src={img} alt=""/>)}</div>
-          .
+        <input className="imgInput" type="file" id="file" accept="image/*" onChange={ChangeImage}/>
+        <div id="img-area" className="box"> {imageUrls.map(img=> <img id="img" src={img} alt="" width='auto' height='300px'/>)}</div>
+        { grayscale &&
+        <input className="rangeInput" type='range' min='0' max='100' step='1' value={slider} onChange={(e)=>setSlider(e.target.value)}/>
+        }
+        <p></p><button className="grayscale" onClick={()=>{
+        {
+          !grayscale &&
+              setSlider(100)
+        }
+          changeGrayscale()
+      }}>Grayscale</button>
       </div>
   );
 }
