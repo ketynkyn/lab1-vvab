b9dff726dce38b60fff14ccebfd3e925c826b474
Valeriy_Voblikov <103932538+ketynkyn@users.noreply.github.com>
2023-04-15 20:09:44 +0300

    qwer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ function App() {
     const[slider2, setSlider2] = useState(0);
     const[slider3, setSlider3] = useState(100);
     const[slider4, setSlider4] = useState(100);
+    const[sliderRed, setsliderRed] = useState(0);
+    const[sliderGreen, setsliderGreen] = useState(0);
+    const[sliderBlue, setsliderBlue] = useState(0);
+    const[Red,setRed] = useState("0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1")
+    const[Green,setGreen] = useState("0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1")
+    const[Blue,setBlue] = useState("0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1")
+
+
 
     const ChangeImage = (event) => {
     setImage([...event.target.files])
@@ -22,8 +30,9 @@ function App() {
     setImageUrls(newImageUrls);
   }, [image]);
 
+
   const changeGrayscale = () => {
-    document.getElementById("img").style.filter="grayscale("+slider+"%)"+"sepia("+slider2+"%)"+"brightness("+slider3+"%)"+"contrast("+slider4+"%)"
+    document.getElementById("img").style.filter="grayscale("+slider+"%)"+"sepia("+slider2+"%)"+"brightness("+slider3+"%)"+"contrast("+slider4+"%)"+"url(#posterize)"
   }
 
     const ChangeFilterImage = (e) => {
@@ -45,12 +54,51 @@ function App() {
         changeGrayscale()
     }
 
+    const ChangeFilterImage5 = (e) => {
+        let count=""
+        for (let i=0.1;i<e;i+=0.1){
+            count=i+' '
+        }
+        setRed(count)
+        setsliderRed(e)
+        changeGrayscale()
+    }
+    const ChangeFilterImage6 = (e) => {
+        let count=""
+        for (let i=0.1;i<e;i+=0.1){
+            count=i+' '
+        }
+        setGreen(count)
+        setsliderGreen(e)
+        changeGrayscale()
+    }
+    const ChangeFilterImage7 = (e) => {
+        let count=""
+        for (let i=0.1;i<e;i+=0.1){
+            count=i+' '
+        }
+        setBlue(count)
+        setsliderBlue(e)
+        changeGrayscale()
+    }
+
+
   return (
+
       <div className="App">
+          <svg width="500" height="0" viewBox="0 0 500 335">
+              <filter id="posterize">
+                  <feComponentTransfer>
+                      <feFuncR type="table" tableValues={Red} />
+                      <feFuncG type="table" tableValues={Green} />
+                      <feFuncB type="table" tableValues={Blue}/>
+                  </feComponentTransfer>
+              </filter>
+          </svg>
           <div className="boxImg">
         <input className="imgInput" type="file" id="file" accept="image/*" onChange={ChangeImage} hidden/>
               <button onClick={()=>document.getElementById("file").click()}>Вставить картинку</button>
-        <div id="img-area" className="box"> {imageUrls.map(img=> <img id="img" src={img} alt="" width='auto' height='300px'/>)}</div>
+        <div id="img-area" className="box"> {imageUrls.map(img=> <img id="img" src={img} alt="" width='auto' height='auto'/>)}</div>
               <button>скачать</button>
           </div>
           <div className="leftPanel">
@@ -66,14 +114,14 @@ function App() {
           <div className="filter">Contrast
               <input className="rangeInput" type='range' min='0' max='200' step='1' value={slider4} onChange={(e)=>ChangeFilterImage4(e.target.value)}/>
           </div>
-              <div className="filter">что-то еще
-                  <input className="rangeInput" type='range' min='0' max='100' step='1' value={0}/>
+              <div className="filter">Ванильный фильтр
+                  <input className="rangeInput" type='range' min='0' max='1' step='0.1' value={sliderRed} onChange={(e)=>ChangeFilterImage5(e.target.value)}/>
               </div>
-              <div className="filter">чтото еще
-                  <input className="rangeInput" type='range' min='0' max='100' step='1' value={0} />
+              <div className="filter">Ванилка2
+                  <input className="rangeInput" type='range' min='0' max='1' step='0.1' value={sliderGreen} onChange={(e)=>ChangeFilterImage6(e.target.value)} />
               </div>
-              <div className="filter">чтото еще
-                  <input className="rangeInput" type='range' min='0' max='300' step='1' value={0} />
+              <div className="filter">Ванилка3
+                  <input className="rangeInput" type='range' min='0' max='1' step='0.1' value={sliderBlue} onChange={(e)=>ChangeFilterImage7(e.target.value)}/>
               </div>
               <div className="filter">чтото еще
                   <input className="rangeInput" type='range' min='0' max='200' step='1' value={0} />
